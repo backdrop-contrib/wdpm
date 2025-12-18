77745cd97c4dc5fed78bde270042ce3015c0cbb9
Richard Peacock <richard@peacocksoftware.com>
2025-12-17 19:44:31 -0600

    Fixed a bug where sometimes a double-click is registered, causing the wdpm output to open and then immediately close. This may have been due to differences in themes? Not sure.

--- js/wdpm.js
+++ js/wdpm.js
@@ -2,10 +2,12 @@ Backdrop.behaviors.wdpmStartup = {
   attach: function (context) {
 
     
-  jQuery('.wdpm-html-print-r-selector').click(function() {
+  jQuery('.wdpm-html-print-r-selector').off('click').on('click', function() {
+    
     var rndid = jQuery(this).data('openup');
     
     jQuery('#wdpm-html-print-r-var-value-' + rndid).slideToggle();
+    
     
   });
     
